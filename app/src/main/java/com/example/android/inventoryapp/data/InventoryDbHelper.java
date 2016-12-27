453f50be0f430fe9298bc0f9881ddb8e54b1344e
Iva <erceg_iva@yahoo.co.uk>
2016-12-27 16:38:02 +0100

    implemented InventoryDbHelper class

---
+++ app/src/main/java/com/example/android/inventoryapp/data/InventoryDbHelper.java
@@ -0,0 +1,41 @@
+package com.example.android.inventoryapp.data;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class InventoryDbHelper extends SQLiteOpenHelper{
+    public static final int DATABASE_VERSION = 1;
+    public static final String DATABASE_NAME = "Store.db";
+    private static final String TEXT_TYPE = " TEXT";
+    private static final String INT_TYPE = " INTEGER";
+    private static final String COMMA_SEP = ",";
+
+    private static final String SQL_CREATE_ENTRIES =
+            "CREATE TABLE " + InventoryContract.InventoryEntry.TABLE_NAME + " (" +
+                    InventoryContract.InventoryEntry._ID + " INTEGER PRIMARY KEY," +
+                    InventoryContract.InventoryEntry.COLUMN_ITEM_NAME + TEXT_TYPE + " NOT NULL," +
+                    InventoryContract.InventoryEntry.COLUMN_ITEM_PRICE + INT_TYPE + " NOT NULL," +
+                    InventoryContract.InventoryEntry.COLUMN_ITEM_QUANTITY + INT_TYPE + " DEFAULT 0," +
+                    InventoryContract.InventoryEntry.COLUMN_ITEM_IMAGE + INT_TYPE + COMMA_SEP +
+                    InventoryContract.InventoryEntry.COLUMN_ITEM_DESCRIPTION + TEXT_TYPE + " NOT NULL)";
+
+    private static final String SQL_DELETE_ENTRIES =
+            "DROP TABLE IF EXISTS " + InventoryContract.InventoryEntry.TABLE_NAME;
+
+    public InventoryDbHelper(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL(SQL_CREATE_ENTRIES);
+    }
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        // This database is only a cache for online data, so its upgrade policy is
+        // to simply to discard the data and start over
+        db.execSQL(SQL_DELETE_ENTRIES);
+        onCreate(db);
+    }
+    public void onDowngrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        onUpgrade(db, oldVersion, newVersion);
+    }
+}
